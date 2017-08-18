c40264d76c2861d6dba50f425aae32acfac4df88
Megan Dollar <megdollar@gmail.com>
2017-08-17 21:05:17 -0400

    add uploads

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import jsonify, url_for, flash
+from flask import send_from_directory
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, Item, User
@@ -13,9 +14,18 @@ import httplib2
 import json
 from flask import make_response
 import requests
+import os
+from werkzeug import secure_filename
+
 
 
 app = Flask(__name__)
+
+# This is the path to the upload directory
+app.config['UPLOAD_FOLDER'] = 'images/uploads/'
+# These are the extension that we are accepting to be uploaded
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 
 
 CLIENT_ID = json.loads(
@@ -28,6 +38,39 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    
+# Route that will process the file upload
+@app.route('/dreams/upload/', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+    # Get the name of the uploaded file
+        file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+        if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+            filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+            return redirect(url_for('uploaded_file',
+                                filename=filename))
+    return render_template('upload.html')
+    
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/dreams/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 # Create a state token to prevent request forgery
