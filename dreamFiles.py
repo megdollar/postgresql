fffbb1569081e8702514f7ef2c74c5ffb2538431
Megan Dollar <megdollar@gmail.com>
2017-08-21 15:50:05 -0400

    Update dreamFiles.py

--- dreamFiles.py
+++ dreamFiles.py
@@ -6,7 +6,7 @@ import calendar
 
 from database_setup import Category, Item, Base, User
 
-engine = create_engine('sqlite:///dreamswithusers.db')
+engine = create_engine('postgresql://catalog:catalog@localhost/dreamApp')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
